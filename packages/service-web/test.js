220c3e710aa1068835f7d1703c6595ba8bf15eae
Adrien HARNAY <adrien@harnay.me>
2022-06-02 14:46:53 +0200

    Update test.js

--- packages/service-web/test.js
+++ packages/service-web/test.js
@@ -2,7 +2,7 @@
 import js from "a.js";
 import web from "a.web";
 
-// Should error (OK)
+// Should error (KO!!!)
 import native from "a.native";
 import ios from "a.ios";
 import android from "a.android";
